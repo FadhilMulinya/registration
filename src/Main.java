8c6dea13b01b42255ad60069dc7ae4bbd91b746b
fadhil <134047249+FadhilMulinya@users.noreply.github.com>
2024-08-02 10:24:24 +0300

    Adding the codes for age/dob areas

--- src/Main.java
+++ src/Main.java
@@ -40,11 +40,29 @@ public class Main{
         ButtonGroup genderGroup = new ButtonGroup(); //creating a button group to group the radiobuttons
         genderGroup.add(maleButton); //adding the male radio buttons to the group
         genderGroup.add(femaleButton); //adding the female radio buttons to the group
-        genderPanel.add(maleButton);
-        genderPanel.add(femaleButton);
-        frame.add(genderLabel);
-        frame.add(genderPanel);
-        
+        genderPanel.add(maleButton); //adding the male radio button to the group
+        genderPanel.add(femaleButton); //adding of the femal radio button to th e group
+        frame.add(genderLabel); //adding of the gender label to the frame
+        frame.add(genderPanel); //adding of the gener panel to the group
+
+        //Adding of the DOB Label and the combo boxes
+        JLabel ageLabel = new JLabel("D.O.B"); //creating a label for the DOB fields
+        JPanel agePanel = new JPanel(new FlowLayout());  //creating a panel with the flowlayout that will hold combo boxes
+        //we will create arrays for days months and years
+        String[] day = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+        String[] month = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        String[] year = {"1985","1986","1987","1988","1989","1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006","2007"};
+        JComboBox<String> dayCombo = new JComboBox<>(day); //creating a combo box for the days
+        JComboBox<String> monthCombo = new JComboBox<>(month); //creating a combo box for the months
+        JComboBox<String> yearCombo = new JComboBox<>(year); //creating a combo box for the year
+        agePanel.add(dayCombo); //adding the day combo box to the panel
+        agePanel.add(monthCombo); //adding the month combo box t the panel
+        agePanel.add(yearCombo); //adding the year combo box to the pannel
+        frame.add(ageLabel);
+        frame.add(agePanel);
+
+
+
 
         
         
